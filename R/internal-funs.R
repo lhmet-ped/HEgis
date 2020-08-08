26a3e41523df64aadc0c584d5780f006c7b8c287
lhmet <lhmet@ufsm.br>
2020-08-08 19:36:30 -0300

    fix deps

--- R/internal-funs.R
+++ R/internal-funs.R
@@ -1,8 +1,10 @@
 
 # Fix var types (to be used in import_bhs_ons())
+#' @importFrom dplyr if_else
 .fix_nas <- function(x) dplyr::if_else(x < 0, NA_real_, x )
 
 # Set 'negative strings' as NA_character (to be used in import_bhs_ons())
+#' @importFrom stringr str_detect
 .fix_nas_char <- function(x) {
   dplyr::if_else(stringr::str_detect(x, "-[9]{1,}"), NA_character_, x)
 }
