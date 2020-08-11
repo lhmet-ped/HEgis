56d92f07fb770ea80ff7313ff5fa6ccc267fcc00
lhmet <lhmet@ufsm.br>
2020-08-11 15:36:38 -0300

    improve doc

---
+++ R/HEgis-package.R
@@ -0,0 +1,19 @@
+#' HEgis: Functions To Import GIS Data Needed For The HYDROENGIE Project
+#'
+#'The goal of HEgis is to prepare GIS data for use in HydroEngie R&D project.
+#'
+#'@section HEgis functions:
+#'The main functions are:
+#' \itemize{
+#'    \item \code{\link{extract_rar}}
+#'    \item \code{\link{import_bhs_ons}}
+#' }
+#'
+#'@docType package
+#'@name HEgis
+#'
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
