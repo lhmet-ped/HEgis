658151c8bed7b651a47ed5ca7515a2dad2d9fbca
lhmet <lhmet@ufsm.br>
2020-09-02 23:27:15 -0300

    use tests

--- tests/testthat/test-import-bhs-ons.R
+++ tests/testthat/test-import-bhs-ons.R
@@ -7,19 +7,45 @@ test_that("test for a inexistent shape file", {
   )
 })
 
+
+
 # TO DO:
 .shape_from_zip_version_of_rar <- function(){
+  # clean-up tmp dirs before tests
+
+  # unlink(
+  #   list.files(dirname(tempdir()),
+  #              pattern = "^R.*",
+  #              full.names = TRUE
+  #   ),
+  #   recursive = TRUE
+  # )
+
   dest_file <- tempfile(fileext = ".zip")
   #load("R/sysdata.rda")
   download.file(zip_url, dest_file, mode = 'wb')
-  dir2extract <- file.path(dirname(dest_file), "BaciasHidrograficasONS_JUNTOS")
-  unzip(dest_file, exdir = dir2extract)
+  # file.exists(dest_file)
+  dir2extract <- file.path(dirname(dest_file), "bacias-ons")
+  #dir2extract <- dirname(dest_file)
+  unzip(dest_file, exdir = dir2extract, overwrite = TRUE)
   #list.files(dirname(dir2extract))
-  shp <- list.files(dir2extract, pattern = "Bacias.*\\.shp$", full.names = TRUE)
+  shp <- list.files(dir2extract,
+                    pattern = "Bacias.*\\.shp$",
+                    full.names = TRUE,
+                    recursive = TRUE)
   return(shp)
 }
 
-test_that("test with expected shape file", {
+test_that("test import shape file from a architeture different from linux", {
+
+  # test with a zip version of original rar file (for tests)
+  bhs_shape <- .shape_from_zip_version_of_rar()
+  bhs_pols <- import_bhs_ons(bhs_shape, quiet = TRUE)
+
+  expect_is(bhs_pols, class = c("sf", "data.frame"))
+})
+
+test_that("test import shape file in linux  and unrar", {
 
   bhs_rar <- system.file(
     "extdata",
@@ -31,16 +57,16 @@ test_that("test with expected shape file", {
     lhmetools:::.check_unrar(TRUE)) {
     library(lhmetools)
     temp_d <- tempdir()
-    shps <- unrar(bhs_rar, dest_dir = temp_d)
+    shps <- unrar(bhs_rar, dest_dir = temp_d, overwrite = TRUE)
     #bhs_shape_file <- shps[grep("Bacias.*\\.shp$", fs::path_file(shps))]
     bhs_shape_file <- shps[grep("Bacias.*\\.shp$", basename(shps))]
     bhs_pols <- import_bhs_ons(bhs_shape_file, quiet = FALSE)
-  } else {
-    # test with a zip version of original rar file (for tests)
-    bhs_shape_file <- .shape_from_zip_version_of_rar()
-    bhs_pols <- import_bhs_ons(bhs_shape_file, quiet = TRUE)
+    expect_is(bhs_pols, class = c("sf", "data.frame"))
+    return(NULL)
   }
-  expect_is(bhs_pols, class = c("sf", "data.frame"))
+
+  expect_error(bhs_pols)
+
 })
 
 
