6fc2e26ec188e1bbf8ca0327d14545e49cc3170a
lhmet <lhmet@ufsm.br>
2020-10-04 22:41:43 -0300

    add description

--- R/data74.R
+++ R/data74.R
@@ -35,17 +35,3 @@
 "poly74"
 
 
-#' Annual climatology of Precipitation over catchment area of ONS station 74.
-#'
-#' As of Sep 2020.
-#'
-#' @format A RasterLayer with total annual precipitation in mm.
-#' \describe{
-#' \item{crs}{coordinate reference system is '+proj=longlat +datum=WGS84'}
-#' \item{dimensions}{14 rows, 19 columns, 266 cells}
-#' \item{resolution}{0.25 arc degrees, ~25 km at equator}
-#' }
-#' @source \url{https://utexas.app.box.com/v/Xavier-etal-IJOC-DATA/}
-#' @references https://rmets.onlinelibrary.wiley.com/doi/full/10.1002/joc.4518
-"precclim74"
-
